77383bc0ae0d6c590a33cf0a7b213b2e65302a75
1-8192 <allegranzia@gmail.com>
2025-03-29 17:27:14 -0400

    solved memoize problem

--- python/memoize.js
+++ python/memoize.js
@@ -63,3 +63,35 @@
 //     actions[i] is one of "call" and "getCallCount"
 //     fnName is one of "sum", "factorial" and "fib"
 
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+    
+    return function(...args) {
+        
+        const key = JSON.stringify(args);
+        
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+    };
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
