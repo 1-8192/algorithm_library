cd0a574be11ba1e52c1a7f567b7e8c7d4691567f
1-8192 <allegranzia@gmail.com>
2025-03-30 16:28:20 -0400

    solved classic binary search

--- python/binary_search.py
+++ python/binary_search.py
@@ -21,3 +21,18 @@
 #     All the integers in nums are unique.
 #     nums is sorted in ascending order.
 
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return -1
