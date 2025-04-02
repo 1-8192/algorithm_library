769bec1ccdd7fc6c2ce2a1275302f933d72298f8
1-8192 <allegranzia@gmail.com>
2025-04-02 17:52:34 -0400

    added word shortener

---
+++ python/shorten_word.py
@@ -0,0 +1,24 @@
+# Given a string input, shorten the word to initial character, the number of letters minus the first
+# and last character, and last character.
+
+# EG:
+# accessibility -> a11y
+# Test this -> t3t t2s
+
+def shorten_word(word):
+    if word == None:
+        return ""
+    
+    words = word.split()
+    results = []
+
+    for i in words:
+        if len(i) < 3:
+            results.append(i)
+        else:
+            results.append(i[0] + str(len(i) -2) + i[-1])
+    
+    return ' '.join(results)
+
+print(shorten_word("accessibility")) # a11y
+print(shorten_word("Test this")) # t3t t2s
