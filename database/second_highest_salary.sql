5b1a5b98eafb7b665191c81a5563d14767926bbe
1-8192 <allegranzia@gmail.com>
2025-04-06 11:04:27 -0400

    solved problem

--- database/second_highest_salary.sql
+++ database/second_highest_salary.sql
@@ -45,3 +45,7 @@
 -- | null                |
 -- +---------------------+
 
+SELECT 
+    (SELECT DISTINCT salary FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET 1) as SecondHighestSalary
