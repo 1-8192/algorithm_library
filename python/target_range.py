318203c243e7c01a796f452b4111dd03c7f22241
1-8192 <allegranzia@gmail.com>
2025-10-27 19:40:49 -0400

    solved range problem

--- python/target_range.py
+++ python/target_range.py
@@ -24,3 +24,58 @@
 #     nums is a non-decreasing array.
 #     -109 <= target <= 109
 
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        """
+        Binary search to reach O(log N) time complexity
+        """
+
+        if not nums:
+            return [-1, -1]
+        
+        left_pos = self.findLeft(nums, target)
+
+        if left_pos == -1:
+            return [-1, -1]
+
+        right_pos = self.findRight(nums, target)
+
+        if right_pos == -1:
+            return [left_pos, left_pos]
+        
+        return [left_pos, right_pos]
+    
+    def findLeft(self, nums, target):
+        result = -1
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                result = mid
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return result
+
+    
+    def findRight(self, nums, target):
+        result = -1
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            if nums[mid] == target:
+                result = mid
+                left = mid + 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return result
