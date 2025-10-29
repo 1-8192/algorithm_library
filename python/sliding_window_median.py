63b5aa8713e620c2d550e34a69a54d924029774e
1-8192 <allegranzia@gmail.com>
2025-10-28 21:05:41 -0400

    slow solution

--- python/sliding_window_median.py
+++ python/sliding_window_median.py
@@ -7,3 +7,26 @@
 
 # Return the median array for each window in the original array. Answers within 10-5 of the actual value will be accepted.
 
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        """
+        Slow solution O(nklog(k))
+        """
+        medians = []
+        i = 0
+        while i <= (len(nums) - k):
+            sub_array = nums[i:i+k]
+            sub_array.sort()
+            if k % 2 == 0:
+                index = k // 2 - 1
+                index_two = k // 2
+                medians.append((sub_array[index_two] + sub_array[index]) / 2)
+            else:
+                index = k // 2
+                medians.append(sub_array[index])
+            i = i + 1
+        
+        return medians
+    
+    def medianSlidingWindowHeap(self, nums: List[int], k: int) -> List[float]:
+        
